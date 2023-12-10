18fe4a4d0ee2c0a9a1378f54657e042969603aee
Lucas-FAlves <Lucas.3451@hotmail.com>
2023-12-10 12:56:35 -0300

    Tentativa de integração front e back

---
+++ back/index.js
@@ -0,0 +1,96 @@
+require("dotenv").config();
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const cors = require('cors');
+const path = require('path');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+//Necessário para extrair os dados de Forms vindos de uma requisição POST
+app.use(express.json());
+app.use(cors());
+
+
+app.listen(3000, () => {
+    console.log('Servidor na porta 3000');
+});
+
+app.post('/Login', async (req,res) => {
+
+    //extraindo os dados do formulário para criacao do usuario
+    const {email, password} = req.body; 
+    
+    //Abre o bd (aqui estamos simulando com arquivo)
+    const jsonPath = path.join(__dirname, '.', 'db', 'banco-dados-usuario.json');
+    const usuariosCadastrados = JSON.parse(fs.readFileSync(jsonPath, { encoding: 'utf8', flag: 'r' }));
+
+    //verifica se existe usuario com email    
+    for (let user of usuariosCadastrados){
+        if(user.email === email){
+            const passwordValidado = await bcrypt.compare(password, user.password);
+            if(passwordValidado){ 
+                
+                const token = jwt.sign(user, process.env.TOKEN);
+
+                return res.json({ "token" : token});
+            }
+            
+            else
+                return res.status(422).send(`Usuario ou senhas incorretas.`);
+        }   
+    }
+    //Nesse ponto não existe usuario com email informado.
+    return res.status(409).send(`Usuario com email ${email} não existe. Considere criar uma conta!`);
+
+})
+
+//Requisicao com POST publica para criar usuário
+app.post('/Cadastro', async (req,res) => {
+    //extraindo os dados do formulário para criacao do usuario
+    const {username, email, password} = req.body; 
+    
+    const jsonPath = path.join(__dirname, '.', 'db', 'banco-dados-usuario.json');
+    const usuariosCadastrados = JSON.parse(fs.readFileSync(jsonPath, { encoding: 'utf8', flag: 'r' }));
+
+    //verifica se já existe usuario com o email informado
+    
+    for (let users of usuariosCadastrados){
+        if(users.email === email){
+            //usuario já existe. Impossivel criar outro
+            //Retornando o erro 409 para indicar conflito
+            return res.status(409).send(`Usuario com email ${email} já existe.`);
+        }   
+    }
+    //Deu certo. Vamos colocar o usuário no "banco"
+    //Gerar um id incremental baseado na qt de users
+    const id = usuariosCadastrados.length + 1;
+    
+    //gerar uma senha cryptografada
+    const salt = await bcrypt.genSalt(10);
+    const passwordCrypt = await bcrypt.hash(password,salt);
+
+    //Criacao do user
+    const user = new User(id, username, email, passwordCrypt);
+
+    //Salva user no "banco"
+    usuariosCadastrados.push(user);
+    fs.writeFileSync(jsonPath,JSON.stringify(usuariosCadastrados,null,2));
+    res.send(`Tudo certo usuario criado com sucesso.`);
+});
+
+function verificaToken(req,res,next){
+
+    const authHeaders = req.headers['authorization'];
+    
+    const token = authHeaders && authHeaders.split(' ')[1]
+    //Bearer token
+
+    if(token == null) return res.status(401).send('Acesso Negado');
+
+    jwt.verify(token, process.env.TOKEN, (err) => {
+        if(err) return res.status(403).send('Token Inválido/Expirado');
+        next();
+    })
+
+}
--- front/src/Inicio/Cadastro.jsx
+++ front/src/Inicio/Cadastro.jsx
@@ -1,17 +1,39 @@
 import "../Styles/login.css"
 import Footer from "./Footer"
 import Header from "./Header"
-
+import { useState } from 'react';
+import axios from 'axios';
+import * as yup from 'yup';
+import {set, useForm} from 'react-hook-form';
 import { Link, Navigate } from 'react-router-dom'
 
 export default function Login(){
+    const [userCriado,setUserCriado] = useState(false);
+    const [msg, setMsg] = useState();
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(e);}
+        
+    const submit = async (data) => {
+        try {
+            const response = await axios.post('http://localhost:3000/Cadastro', data);
+            setMsg(response.data);
+            if(response.data.includes('sucesso'))
+                setUserCriado(true);
+        } catch (error) {
+            setMsg(error.response.data);
+        }   
+    }
+
+
     return (
         <>
         <Header/>
         <main className="login">
         <div className="container-register">
             <h3>Sign out</h3>
-            <form action="" method="post">
+            <form action="http://localhost:5173/Cadastro" method="post" onSubmit={handleSubmit(submit)}>
                 <div className="form-group-login">
                     <label htmlFor="username">Username</label>
                     <input type="text" id="username" name="username" required/>
