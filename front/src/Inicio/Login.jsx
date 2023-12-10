13a9f55da4b7d19f09e3f3f0fa6372f2f3a0d887
Lucas-FAlves <Lucas.3451@hotmail.com>
2023-12-10 13:20:01 -0300

    Login ABRINDO tbm AEEEEEEEEEEEEEEEEEEEEEEE

--- front/src/Inicio/Login.jsx
+++ front/src/Inicio/Login.jsx
@@ -1,17 +1,53 @@
 import "../Styles/login.css"
 import Footer from "./Footer"
 import Header from "./Header"
-
+import {useForm} from 'react-hook-form';
+import axios from 'axios';
+import * as yup from 'yup';
+import {yupResolver} from '@hookform/resolvers/yup';
+import { useState } from 'react';
 import { Link, Navigate } from 'react-router-dom';
 
 export default function Login(){
+    const schema = yup.object({
+        email: yup.string().email('Email inválido').required('Email obrigatório'),
+        password: yup.string().min(2,'Campo Senha Obrigatório').required(),
+    });
+
+    const [msg, setMsg] = useState(' ');
+
+    const form = useForm({
+        resolver: yupResolver(schema)
+    });
+
+    const { register, handleSubmit, formState } = form;
+
+    const {errors} = formState;
+
+    const submit = async (data) => {
+        try {
+            const response = await axios.post('http://localhost:3000/login', data);
+            //Extrair o token
+            const token = response.data.token;
+            sessionStorage.setItem('token', token);
+            if(token)
+                setMsg('Autenticado');
+        } catch (error) {
+            setMsg(error.response.data);
+        }   
+    }
+
+    if(msg.toLowerCase().includes('autenticado')){
+        return <Navigate to='/caminho' />
+    }
+
     return (
         <>
         <Header/>
         <main className="login">
         <div className="container-login">
             <h3 className="sign">SIGN IN</h3>
-            <form action="post" method="post">
+            <form action="post" method="post" onSubmit={handleSubmit(submit)} noValidate>
                 <div className="form-group-login">
                     <label htmlFor="username">Username or Email</label>
                     <input type="text" id="username" name="username" required/>
