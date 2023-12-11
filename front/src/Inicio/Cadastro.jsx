c5878f8817d9b2c10e8a33f81916e6f640e58242
Kazee1 <gabriel_cocovilo20@outlook.com>
2023-12-11 10:58:37 -0300

    toma ai

--- front/src/Inicio/Cadastro.jsx
+++ front/src/Inicio/Cadastro.jsx
@@ -1,78 +1,118 @@
-import "../Styles/login.css"
-import Footer from "./Footer"
-import Header from "./Header"
-import { useState } from 'react';
-import axios from 'axios';
-import * as yup from 'yup';
-import {set, useForm} from 'react-hook-form';
-import {yupResolver} from '@hookform/resolvers/yup';
-import { Link, Navigate } from 'react-router-dom'
+import "../Styles/login.css";
+import Footer from "./Footer";
+import Header from "./Header";
+import { useState } from "react";
+import axios from "axios";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Link, Navigate } from "react-router-dom";
 
-export default function Login(){
-    const [userCriado,setUserCriado] = useState(false);
-    const [msg, setMsg] = useState();
-    const form = useForm();
-    // const schema = yup.object().shape({
-    //     username: yup.string().required('Usuário obrigatório'),
-    //     email: yup.string().email('Email inválido').required('Email obrigatório'),
-    //     password: yup.string().min(2, 'Senha com no mínimo 2 caracteres').required('Senha obrigatória'),
-    //     passwordConf: yup.string()
-    //       .oneOf([yup.ref('password')], 'As senhas devem coincidir')
-    //       .required('Confirme a senha'),
-    //   });
-      
-    //   const form = useForm({
-    //     resolver: yupResolver(schema),
-    //   });
+export default function Cadastro() {
+  const [userCriado, setUserCriado] = useState(false);
+  const [msg, setMsg] = useState();
 
-    const { register, control, handleSubmit, formState } = form;
-    // const { register, handleSubmit, formState } = form;
-    const {errors} = formState;
+  const schema = yup.object().shape({
+    username: yup.string().required("Usuário obrigatório"),
+    email: yup.string().email("Email inválido").required("Email obrigatório"),
+    password: yup
+      .string()
+      .min(2, "Senha com no mínimo 2 caracteres")
+      .required("Senha obrigatória"),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref("password")], "As senhas devem coincidir")
+      .required("Confirme a senha"),
+  });
 
-    const submit = async (data) => {
-        try {
-            const response = await axios.post('http://localhost:3000/Cadastro', data);
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const { errors } = formState;
+
+  const submit = async (data) => {
+    try {
+      await handleSubmit((data) => {
+        console.log("Dados a serem enviados:", data);
+        axios
+          .post("http://localhost:3000/Cadastro", data)
+          .then((response) => {
             setMsg(response.data);
-            if(response.data.includes('sucesso'))
-                setUserCriado(true);
-        } catch (error) {
+            if (response.data.includes("sucesso")) {
+              setUserCriado(true);
+              setTimeout(() => {
+                window.location.href = "/login";
+              }, 2000);
+            }
+          })
+          .catch((error) => {
             setMsg(error.response.data);
-        }   
+          });
+      })(data);
+    } catch (error) {
+      console.error(error);
     }
-
-
-    return (
-        <>
-        <Header/>
-        <main className="login">
+  };
+  return (
+    <>
+      <Header />
+      <main className="login">
         <div className="container-register">
-            <h3>Sign out</h3>
-            <form onSubmit={handleSubmit(submit)} noValidate>
-                <div className="form-group-login">
-                    <label htmlFor="username" placeholder="username">Username</label>
-                    <input type="text" id="username" {...register('username')}/>
-                </div>
-                <div className="form-group-login">
-                    <label htmlFor="Email" placeholder="Email">Email</label>
-                    <input type="text" id="Email" {...register('email')}/>
-                </div>
-                <div className="form-group-login">
-                    <label htmlFor="password">Password</label>
-                    <input type="password" id="password" {...register('password')}/>
-                </div>
-                <div className="form-group-login">
-                    <label htmlFor="confirmPassword">Confirm Password</label>
-                    <input type="password" id="confirmPassword" {...register('confirmPassword')}/>
-                </div>
-                <button className="btn">Loginaaaf</button>
-            </form>
-            <p className='server-response'>{msg}</p>
-            <div className="links">
-                <p>Already have an account? <Link to='/Login' className='link'>Sign in</Link></p>
+          <h3>Sign out</h3>
+          <form onSubmit={handleSubmit(submit)} noValidate>
+            <div className="form-group-login">
+              <label htmlFor="username" placeholder="username">
+                Username
+              </label>
+              <input type="text" id="username" {...register("username")} />
+              {errors.username && (
+                <p className="error-message">{errors.username.message}</p>
+              )}
             </div>
+            <div className="form-group-login">
+              <label htmlFor="Email" placeholder="Email">
+                Email
+              </label>
+              <input type="text" id="Email" {...register("email")} />
+              {errors.email && (
+                <p className="error-message">{errors.email.message}</p>
+              )}
+            </div>
+            <div className="form-group-login">
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" {...register("password")} />
+              {errors.password && (
+                <p className="error-message">{errors.password.message}</p>
+              )}
+            </div>
+            <div className="form-group-login">
+              <label htmlFor="confirmPassword">Confirm Password</label>
+              <input
+                type="password"
+                id="confirmPassword"
+                {...register("confirmPassword")}
+              />
+              {errors.confirmPassword && (
+                <p className="error-message">
+                  {errors.confirmPassword.message}
+                </p>
+              )}
+            </div>
+            <button className="btn">Register</button>
+          </form>
+          <p className="server-response">{msg}</p>
+          <div className="links">
+            <p>
+              Already have an account?{" "}
+              <Link to="/Login" className="link">
+                Sign in
+              </Link>
+            </p>
+          </div>
         </div>
-        </main>
-        <Footer/>
-        </>
-    )
+      </main>
+      <Footer />
+    </>
+  );
 }
--- front/src/Inicio/Login.jsx
+++ front/src/Inicio/Login.jsx
@@ -1,47 +1,44 @@
 import "../Styles/login.css"
 import Footer from "./Footer"
 import Header from "./Header"
-import {useForm} from 'react-hook-form';
 import axios from 'axios';
 import * as yup from 'yup';
 import {yupResolver} from '@hookform/resolvers/yup';
 import { useState } from 'react';
+import {useForm} from 'react-hook-form';
 import { Link, Navigate } from 'react-router-dom';
 
 export default function Login(){
-    const form = useForm();
-    // const schema = yup.object({
-    //     email: yup.string().email('Email inválido').required('Email obrigatório'),
-    //     password: yup.string().min(2,'Campo Senha Obrigatório').required(),
-    // });
+    
+    const schema = yup.object().shape({
+        email: yup.string().email('Email inválido').required('Email obrigatório'),
+        password: yup.string().min(2, 'Campo Senha Obrigatório').required('Senha obrigatória'),
+      });
+    
+      const { register, handleSubmit, formState } = useForm({
+        resolver: yupResolver(schema),
+      });
+    
+      const [msg, setMsg] = useState('');
+      const { errors } = formState;
 
-    const [msg, setMsg] = useState(' ');
-
-    // const form = useForm({
-    //     resolver: yupResolver(schema)
-    // });
-
-    const { register,control, handleSubmit, formState } = form;
-
-    const {errors} = formState;
-
-    const submit = async (data) => {
+      const submit = async (data) => {
         try {
+          const response = await form.trigger();
+          if (Object.keys(errors).length === 0) {
             const response = await axios.post('http://localhost:3000/login', data);
-            //Extrair o token
-            const token = response.data.token;
-            sessionStorage.setItem('token', token);
-            if(token)
-                setMsg('Autenticado');
+            if (response && response.data && response.data.token) {
+              const token = response.data.token;
+              sessionStorage.setItem('token', token);
+              setMsg('Autenticado');
+            } else {
+              setMsg('Erro ao autenticar');
+            }
+          }
         } catch (error) {
-            setMsg(error.response.data);
-        }   
-    }
-
-    if(msg.toLowerCase().includes('autenticado')){
-        return <Navigate to='/inicio' />
-    }
-
+          setMsg(error.response?.data || 'Erro ao realizar a requisição');
+        }
+      };
     return (
         <>
         <Header/>
@@ -50,12 +47,14 @@ export default function Login(){
             <h3 className="sign">SIGN IN</h3>
             <form onSubmit={handleSubmit(submit)} noValidate>
                 <div className="form-group-login">
-                    <label htmlFor="username" >Username</label>
-                    <input type="text" id="username" {...register('username')}/>
+                    <label htmlFor="username" >Email</label>
+                    <input type="text" id="username" {...register('email')}/>
+                    {errors.email && <p className="error-message">{errors.email.message}</p>}
                 </div>
                 <div className="form-group-login">
                     <label htmlFor="password">Password</label>
                     <input type="password" id="password" {...register('password')}/>
+                    {errors.password && <p className="error-message">{errors.password.message}</p>}
                 </div>
                 <button className="btn">Sign in</button>
             </form>
