7ca25ad034eb8114e99a488425e4e9be7b7410ca
teukuka <teukuka.silva@hotmail.com>
2023-12-11 22:27:17 -0300

    tudo
    tudo até agora fichas + tokens

--- front/src/FichaBuilder/Fichas/DungeonsDragons.jsx
+++ front/src/FichaBuilder/Fichas/DungeonsDragons.jsx
@@ -4,7 +4,7 @@ import "../../Styles/Fichas/DungeonsDragons.css";
 import "../../Styles/Fichas/Dados.css";
 
 import {set, useForm} from 'react-hook-form';
-import axios, * as others from 'axios';
+//import axios, * as others from 'axios';
 
 import Imagem1 from "../../Imagens/Fichas/DungeonsDragons_Ficha_5E_page1.jpg";
 import Imagem2 from "../../Imagens/Fichas/DungeonsDragons_Ficha_5E_page2.jpg";
